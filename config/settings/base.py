edde8afb5ebda5b4373f35ec484d27a27aad8f50
STQT <productgsm@gmail.com>
2024-08-27 22:50:29 +0500

    deploy

--- config/settings/base.py
+++ config/settings/base.py
@@ -11,9 +11,7 @@ APPS_DIR = BASE_DIR / "chop_geo"
 env = environ.Env()
 
 READ_DOT_ENV_FILE = env.bool("DJANGO_READ_DOT_ENV_FILE", default=False)
-if READ_DOT_ENV_FILE:
-    # OS environment variables take precedence over variables from .env
-    env.read_env(str(BASE_DIR / ".env"))
+env.read_env(str(BASE_DIR / ".env"))
 
 # GENERAL
 # ------------------------------------------------------------------------------

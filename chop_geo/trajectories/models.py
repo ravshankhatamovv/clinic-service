68bd381d8d673183dbb77e3b12c5120eb3686f29
STQT <productgsm@gmail.com>
2024-09-20 10:41:56 +0500

    username show

--- chop_geo/trajectories/models.py
+++ chop_geo/trajectories/models.py
@@ -11,26 +11,26 @@ class Vehicle(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="vehicle")
 
     def __str__(self):
-        return str(self.user.name)
+        return str(self.user.username)
 
 
 class VehicleTrajectory(models.Model):
-    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, related_name='trajectories')
+    vehicle = models.ForeignKey("Vehicle", on_delete=models.CASCADE, related_name='trajectories')
     timestamp = models.DateTimeField()
     location = geomodels.PointField()
 
     def __str__(self):
-        return f'{str(self.vehicle.user.name)} - {self.timestamp}'
+        return f'{str(self.vehicle.user.username)} - {self.timestamp}'
 
 
 class VehicleTrajectoryRoute(models.Model):
-    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, related_name='routes')
+    vehicle = models.ForeignKey("Vehicle", on_delete=models.CASCADE, related_name='routes')
     trajectory = geomodels.LineStringField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
 
     def __str__(self):
-        return f'Route for {str(self.vehicle.user.name)} from {self.start_time} to {self.end_time}'
+        return f'Route for {str(self.vehicle.user.username)} from {self.start_time} to {self.end_time}'
 
     @staticmethod
     def generate_trajectory(vehicle):

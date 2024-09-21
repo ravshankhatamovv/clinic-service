c4417ff7557ac0e067cc39029545bd5f79acdea8
STQT <productgsm@gmail.com>
2024-09-21 23:01:32 +0500

    update forms

--- chop_geo/trajectories/forms.py
+++ chop_geo/trajectories/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.gis import forms as gis_forms
-from .models import VehicleTrajectory
+from .models import VehicleTrajectory, VehicleTrajectoryRoute
 
 
 class VehicleTrajectoryForm(forms.ModelForm):
@@ -15,4 +15,21 @@ class VehicleTrajectoryForm(forms.ModelForm):
                 'default_lat': 41.3078118,  # Set default latitude
                 'default_zoom': 12,
             })
+        }
+
+
+class VehicleTrajectoryRouteForm(forms.ModelForm):
+    class Meta:
+        model = VehicleTrajectoryRoute
+        fields = '__all__'
+        widgets = {
+            'trajectory': gis_forms.OSMWidget(attrs={
+                'map_width': 800,
+                'map_height': 500,
+                'default_lon': 69.2374775,  # Set default longitude
+                'default_lat': 41.3078118,  # Set default latitude
+                'default_zoom': 12,
+            }),
+            'start_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'end_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
         }
